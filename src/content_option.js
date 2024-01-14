b6f0034fe9d0eba946af0a363e521cbc3c9e71be
Hamza-abid-dev <0zerogamingyt0@gmail.com>
2024-01-15 03:18:12 +0500

    first commit

--- src/content_option.js
+++ src/content_option.js
@@ -1,69 +1,69 @@
-const logotext = "JOHN";
+const logotext = "Hamza";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Hamza Abid",
+    description: "I’m Hamza Abid Full stack devloper",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Hamza Abid",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
         third: "I develop mobile apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Expert in MERN stack, seamlessly blending MongoDB, Express.js, React.js, and Node.js to craft scalable and immersive web applications. A Full Stack developer with a passion for building end-to-end solutions, ensuring top-notch performance and user experiences.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "Hello! I'm Hamza Abid, a passionate Full Stack Web Developer with a love for turning ideas into interactive and seamless digital experiences. Armed with a robust skill set in both front-end and back-end technologies, I specialize in crafting intuitive user interfaces using modern frameworks like React.js. My expertise extends to server-side development, database management, and API design, allowing me to bring a holistic approach to web development projects. From concept to deployment, I thrive on creating innovative, scalable, and high-performance solutions that make a lasting impact. Let's build something amazing together!",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Developer",
+        where: "Pakistan",
+        date: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Front End Web Developer",
+        where: "Pakistan",
+        date: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Full Stack Mern Developer",
+        where: "Pakistan",
+        date: "2023",
     },
 ];
 
 const skills = [{
-        name: "Python",
+        name: "Html/Css",
         value: 90,
     },
     {
-        name: "Djano",
+        name: "Bootstrap/Tailwind",
         value: 85,
     },
     {
-        name: "Javascript",
-        value: 80,
+        name: "Javascript/React",
+        value: 70,
     },
     {
-        name: "React",
-        value: 60,
+        name: "Node",
+        value: 70,
     },
     {
-        name: "Jquery",
+        name: "Express/MongoDB",
         value: 85,
     },
 ];
 
 const services = [{
-        title: "UI & UX Design",
+        title: "Full Stack Mern Developer",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
     },
     {
-        title: "Mobile Apps",
+        title: "Web Apps",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
     },
     {
@@ -136,21 +136,22 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "hachohan0987@gmail.com",
+    YOUR_FONE: "(92)302-4579819",
+    description: "Reach out to us effortlessly! Whether you have questions, feedback, or want to explore collaboration opportunities, our dedicated team is here to assist you. Connect with us for personalized and prompt support, and let's embark on a journey to enhance your digital experience together.",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_SERVICE_ID: "service_641dfuj",
+    YOUR_TEMPLATE_ID: "template_hdgy2hq",
+    // YOUR_USER_ID: "user_id",
+    YOUR_PUBLIC_KEY: "jEDHb_ZtdIW4uthcE"
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/Hamza-abid-dev",
+    facebook: "https://www.facebook.com/Hamza.Abid.Chohan",
+    linkedin: "https://www.linkedin.com/in/hamza-abid-827b48294/",
+    twitter: "https://twitter.com/NoOne1137481",
 };
 export {
     meta,
--- src/header/index.js
+++ src/header/index.js
@@ -18,7 +18,7 @@ const Headermain = () => {
       <header className="fixed-top site__header">
         <div className="d-flex align-items-center justify-content-between">
           <Link  className="navbar-brand nav_ac" to="/">
-            {logotext}
+            Hamza Abid
           </Link>
           <div className="d-flex align-items-center">
           <Themetoggle />
